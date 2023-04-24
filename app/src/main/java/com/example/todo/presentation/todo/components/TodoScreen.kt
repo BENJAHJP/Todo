2f87147eef42436001dee6fedc84dca3aa3bd5ec
benjahjp <benjahjp@gmail.com>
2023-04-24 12:25:06 +0300

    fix: search

--- app/src/main/java/com/example/todo/presentation/todo/components/TodoScreen.kt
+++ app/src/main/java/com/example/todo/presentation/todo/components/TodoScreen.kt
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -122,6 +124,9 @@ fun TodoScreen(
                                 }
                                 .padding(16.dp)
                         )
+                    }
+                    item { 
+                        Spacer(modifier = Modifier.height(20.dp))
                     }
                 }
             }
